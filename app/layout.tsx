4aa7a767c83f76dfc7fdfff51cfbba16dbd4f671
Karim Diab <141320871+Horuss111@users.noreply.github.com>
2026-03-10 04:43:49 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "MediBook",
+  description: "Premium medical booking website",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
